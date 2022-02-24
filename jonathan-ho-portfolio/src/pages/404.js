239f87d6043727caf08b540b8da01c979b4a0e5a
jonathgh <jonathangho@protonmail.com>
2022-02-24 11:43:16 +0100

    updated files

---
+++ jonathan-ho-portfolio/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function () {
+  return (
+      <div>
+        <h2>404</h2>
+        <h3>Sorry, this page does not exist</h3>
+      </div>
+  )
+}
---
+++ jonathan-ho-portfolio/src/pages/projects/project_01.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function project_01() {
+  return (
+    <div>project_01</div>
+  )
+}
