9a085b9d744be12bc90bf2744c258e3d95290b97
jonathgh <jonathangho@protonmail.com>
2022-02-26 22:42:18 +0100

    updated files

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,6 +3,7 @@ import "../styles/global.css"
 import Layout from "../components/Layout.js"
 import ContentItem from "../components/ContentItem"
 import SunsetShader from "../components/SunsetShader.js"
+import Blob from '../components/BlobShader.js'
 import projectThumb01 from '../imgs/proj-thumb-01.jpg';
 
 
@@ -25,7 +26,7 @@ import projectThumb01 from '../imgs/proj-thumb-01.jpg';
                             <ContentItem thumbSrc={projectThumb01} thumbAlt={"project thumbnail"} description={"Proident ullamco laborum reprehenderit anim cillum. Minim incididunt aliquip ipsum sunt qui eiusmod irure consectetur labore incididunt commodo elit adipisicing. Eiusmod cillum laborum laboris adipisicing do dolor sunt adipisicing occaecat id aliqua. Irure do voluptate minim reprehenderit. Ullamco cupidatat duis Lorem velit nisi aliqua est dolore ut excepteur id laborum anim adipisicing."} />
                         </div>
                     </div>
-                    <SunsetShader />   
+                    <Blob />   
             </div>
         </div>
     )
